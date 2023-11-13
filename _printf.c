8ba9a2134844d45a93000d8ba7bd8bdd9f6dcee5
Thobeats <tobiy23@gmail.com>
2023-11-13 02:08:17 +0100

    update printf

---
+++ _printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _printf - Prints an output according to format
+ *
+ * @format: The format
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	/**int charLength;
+	va_list args;
+	int i, j;
+	char *seperator;*/
+
+	if (format == NULL)
+		return (0);
+
+	return (1);
+}
