1a4d20d87c4cf74a2721c6691d1cdabc45046661
Thobeats <tobiy23@gmail.com>
2023-11-13 01:41:37 +0100

    update

--- printint.c
+++ printint.c
@@ -20,47 +20,14 @@ int print_int(va_list args)
 
 	if (last < 0)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar (digit = '0');
-			num = num - (digit - exp);
-			exp = exp / 10;
-			i++;
-		}
-	_putchar (last + '0');
-	return (i);
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
 	}
 
-}
-
-#include "main.h"
-/**
-* print_dec - prints integer
-* @args: expected arguement
-* Return: Number of characters printed
-*/
-
-
-
-int print_dec(va_list args)
-{
-	int n = va_arg(args, int);
-
-	int num, last = n % 10, digit, exp = 1;
-
-	int i = 1;
-
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	if (last > 0)
 	{
 		while (num / 10 != 0)
 		{
@@ -76,8 +43,54 @@ int print_dec(va_list args)
 			exp = exp / 10;
 			i++;
 		}
+	}
 	_putchar (last + '0');
 	return (i);
-	}
+}
 
+#include "main.h"
+/**
+* print_dec - prints integer
+* @args: expected arguement
+* Return: Number of characters printed
+*/
+
+int print_dec(va_list args)
+{
+	int n = va_arg(args, int);
+
+	int num, last = n % 10, digit, exp = 1;
+
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (last > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar (digit = '0');
+			num = num - (digit - exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar (last + '0');
+	return (i);
 }
