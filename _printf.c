18d28f60c415f65d26779bc4e69c81efefeb68cb
Thobeats <tobiy23@gmail.com>
2023-11-13 03:08:22 +0100

    update printf

--- _printf.c
+++ _printf.c
@@ -12,36 +12,43 @@ int _printf(const char *format, ...)
 {
 	int i;
 	va_list args;
+	int char_length;
 
 	if (format == NULL)
 		return (0);
 
 	va_start(args, format);
 	i = 0;
+	char_length = 0;
 	while (format[i])
 	{
 		if (format[i] != '%')
 		{
 			_putchar(format[i]);
+			char_length++;
 		}
 		else
 		{
 			i++;
 
+			if (format[i] == '\0')
+				break;
+
 			/**Check for characters */
 			if (format[i] == 'c')
 			{
-				printchar(args);
+				char_length += printchar(args);
 			}
 
 			if (format[i] == 's')
 			{
-				printstr(args);
+				char_length += printstr(args);
 			}
 
 			if (format[i] == '%')
 			{
 				print_37();
+				char_length++;
 			}
 		}
 		i++;
