ec13c1287a383a9fe10e31bb48f79e2ad9052209
Thobeats <tobiy23@gmail.com>
2023-11-13 21:42:58 +0100

    update

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' &&!format[2])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(args, format);
 	while (format[i] && format[i] != '\0')
